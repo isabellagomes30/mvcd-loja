b1bea0d1ad1f46064c373a6eb5b4fb9e45c40713
1831364 <1831364@LABCMAQ15.itp.local>
2019-03-25 10:40:20 -0300

    Lista GET

---
+++ lista5/ex1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title>Exercício 1</title>
+        <meta charset="utf-8">
+        
+    </head>
+    <body>
+        
+        <a href="exercicio1.php?v1=10&v2=12&v3=23&v4=32&v5=5">exercício1</a>
+        
+    </body>
+</html>
+
